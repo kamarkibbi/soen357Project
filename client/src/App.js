00b40e568d8d14531b0857510b69e407eef248e6
SolomonMoulangLewis <SOLOMON.LEWIS21@bathspa.ac.uk>
2023-04-15 18:12:09 -0400

    added productivity page

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,11 @@ import './App.css';
 import Login from './components/js/login';
 import SignUpOne from './components/js/signUpOne';
 import SignUpTwo from './components/js/signUpTwo';
+import Productivity from './components/js/productivity';
 
 function App() {
   return (
-    <SignUpTwo />
+    <Productivity />
   );
 }
 
