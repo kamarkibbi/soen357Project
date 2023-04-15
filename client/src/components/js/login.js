936121fc83fa5270994a69adb961348bb57a8408
SolomonMoulangLewis <SOLOMON.LEWIS21@bathspa.ac.uk>
2023-04-15 14:47:46 -0400

    node modules

--- client/src/components/js/login.js
+++ client/src/components/js/login.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 import '../css/main.css'
 import '../css/login.css'
@@ -8,50 +9,60 @@ import Header from './header';
 import logo from '../../img/daylee-logo.png'
 
 function Login() {
+  const formRef = React.useRef(null);
+
+  function submitForm(e) {
+    e.preventDefault();
+
+    const formData = new FormData(formRef.current);
+
+    console.log([...formData]);
+
+    /* axios.post('/submit', formData)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      }); */
+    }
+
   return (
     <>
     <Header/>
       <main>
-        <section class="sign-up">
+        <section className="sign-up">
+
             <h1 id="main-title">Daylee</h1>
 
-            <div class="form-container">
-                <h2 class="title">Sign Up</h2>
-                <form action="">
-                    <div class="form-item">
-                        <label for="first-name">first name</label>
-                        <input type="text" id="first-name"></input>
+            <div className="form-container">
+                <h2 className="title">Sign Up</h2>
+                <form ref={formRef} onSubmit={submitForm}>
+                    <div className="form-item">
+                        <label htmlFor="first-name">first name</label>
+                        <input name='first-name' type="text" id="first-name"></input>
                     </div>
-                    <div class="form-item">
-                        <label for="last-name">last name</label>
-                        <input type="text" id="last-name"></input>
+                    <div className="form-item">
+                        <label htmlFor="last-name">last name</label>
+                        <input name='last-name' type="text" id="last-name"></input>
                     </div>
-                    <div class="form-item">
-                        <label for="email">email</label>
-                        <input type="text" id="email"></input>
+                    <div className="form-item">
+                        <label htmlFor="email">email</label>
+                        <input name='email' type="text" id="email"></input>
                     </div>
-                    <div class="form-item">
+                    <div className="form-item">
                         <button id='submit'>SUBMIT</button>
                     </div>
                 </form>
             </div>
-
         </section>
 
-        <section class="img-container">
-
+        <section className="img-container">
             <img src={logo} alt="an image of a person pushing a clock"></img>
-
         </section>
     </main>
-
     </>
   );
 }
-
-const form = document.querySelector('form');
-const firstName = document.querySelector('#first-name');
-const lastName = document.querySelector('#last-name');
-const submitBtn = document.querySelector('#submit');
 
 export default Login;
