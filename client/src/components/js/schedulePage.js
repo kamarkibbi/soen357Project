ea4a338ea78c3d58bde8c849e954ea63ea6b97e1
SolomonMoulangLewis <SOLOMON.LEWIS21@bathspa.ac.uk>
2023-04-19 16:49:01 -0400

    updates

--- client/src/components/js/schedulePage.js
+++ client/src/components/js/schedulePage.js
@@ -2,17 +2,18 @@ import '../css/main.css'
 import '../css/productivity.css'
 import Header from './header';
 import ScheduleTemp from './scheduleTemp';
+import Timer from './timer';
 
 function SchedulePage() {
     return(
         <>
         <Header/>
         <main>
+        <Timer/>  
             <section className="">
                 <ScheduleTemp/>
 
             </section>
-
         </main>
         </>
     );
